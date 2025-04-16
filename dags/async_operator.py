7850cf4504c6c1b60f22fb5844f1a0e3fd4b20b9
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2025-04-16 17:08:03 +0530

    Update async_operator.py sleep 20

--- dags/async_operator.py
+++ dags/async_operator.py
@@ -13,8 +13,8 @@ default_args = {"start_date": days_ago(0)}
 with DAG("async_operator", schedule_interval="@daily", default_args=default_args, tags=["core", "async", "extended_dags"]) as dag:
     sensor = TimeSensorAsync(
         task_id="sensor",
-        target_time=(utcnow() + datetime.timedelta(seconds=10)).time(),
+        target_time=(utcnow() + datetime.timedelta(seconds=20)).time(),
     )
-    task1 = BashSensor(task_id="sleep_10", bash_command="sleep 10")
+    task1 = BashSensor(task_id="sleep_20", bash_command="sleep 20")
     complete = DummyOperator(task_id="complete")
     sensor >> task1 >> complete
